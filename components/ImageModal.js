75930f400fdca89142557468f6913abbcc45b301
Sebastian Andreasson <sebastianandreasson@MacBook-Pro-2.local>
2023-03-23 16:58:00 -0600

    forgot one file

--- components/ImageModal.js
+++ components/ImageModal.js
@@ -1,21 +1,21 @@
-import { Button, Flex, Image, Modal, Text } from '@mantine/core'
+import {
+  Badge,
+  Button,
+  Card,
+  Flex,
+  Image,
+  Modal,
+  SimpleGrid,
+  Text,
+} from '@mantine/core'
 import { useAtom, useAtomValue } from 'jotai'
 import React from 'react'
-import {
-  modalImageAtom,
-  seedAtom,
-  sequenceAtom,
-  settingsAtom,
-} from '../src/state'
+import { modalImageAtom, seedAtom, settingsAtom } from '../src/state'
 
 const ImageModal = () => {
   const [modalImage, setModalImage] = useAtom(modalImageAtom)
-  const [{ from, to }, setSequence] = useAtom(sequenceAtom)
   const seed = useAtomValue(seedAtom)
   const settings = useAtomValue(settingsAtom)
-
-  const isFrom = JSON.stringify(modalImage) === JSON.stringify(from)
-  const isTo = JSON.stringify(modalImage) === JSON.stringify(to)
 
   return (
     <Modal
@@ -25,33 +25,22 @@ const ImageModal = () => {
       size={600}
     >
       {modalImage && (
-        <Flex direction="column" align="center">
+        <Flex direction="column" align="center" gap="md">
           <Image width={512} height={512} src={modalImage.image} radius="md" />
-          <Text>
+          <SimpleGrid gap="xs" cols={3}>
             {modalImage.prompts
               .map((p, i) => `${p} (${modalImage.weights[i].toFixed(5)})`)
-              .join('\n')}
-            {`, seed: ${seed}, cfg: ${settings.cfg}, steps: ${settings.steps}`}
+              .map((t) => (
+                <Card p={4} withBorder radius="lg">
+                  <Text align="center">
+                    <strong>{t.split(' ')[0]}</strong> {t.split(' ')[1]}
+                  </Text>
+                </Card>
+              ))}
+          </SimpleGrid>
+          <Text align="center">
+            {`seed: ${seed}  cfg: ${settings.cfg} steps: ${settings.steps}`}
           </Text>
-          <Flex gap="md">
-            <Button
-              onClick={() =>
-                setSequence((s) => ({ ...s, from: isFrom ? null : modalImage }))
-              }
-              color={isFrom ? 'red' : 'blue'}
-            >
-              {isFrom ? 'clear' : 'Set from'}
-            </Button>
-
-            <Button
-              onClick={() =>
-                setSequence((s) => ({ ...s, to: isTo ? null : modalImage }))
-              }
-              color={isTo ? 'red' : 'blue'}
-            >
-              {isTo ? 'clear' : 'Set to'}
-            </Button>
-          </Flex>
         </Flex>
       )}
     </Modal>
