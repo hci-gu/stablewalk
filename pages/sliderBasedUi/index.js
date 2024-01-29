7f0e963d7bfe0eacb6b28692613ba827d7553d6a
Josef Forkman <strand.vatten@outlook.com>
2024-01-29 10:58:48 +0100

    Add BasePromptInput component and PromptModal component

--- pages/sliderBasedUi/index.js
+++ pages/sliderBasedUi/index.js
@@ -35,6 +35,114 @@ export const ImgGetter = () => {
   }, [prompts, seed, basePrompt])
 }
 
+const BasePromptInput = () => {
+  const [{ basePrompt }, set] = useAtom(settingsAtom)
+  return (
+    <>
+      <form>
+        <TextInput
+          w={'70vh'}
+          placeholder="dog"
+          value={basePrompt}
+          onChange={(e) => {
+            set({ basePrompt: e.target.value })
+          }}
+        />
+      </form>
+    </>
+  )
+}
+
+;[
+  {
+    basePrompt: '4k photo of a cat',
+    promptsArray: [
+      { id: 0, label: 'Cute', weight: 0 },
+      { id: 1, label: 'Fluffy', weight: 0 },
+      { id: 2, label: 'Orange fur', weight: 0 },
+    ],
+  },
+  {
+    basePrompt: '4k photo of a bike',
+    promptsArray: [
+      { id: 0, label: 'fast', weight: 0 },
+      { id: 1, label: 'big', weight: 0 },
+      { id: 2, label: 'red', weight: 0 },
+    ],
+  },
+]
+
+export const PromptModal = ({ opened, close }) => {
+  const promptStorge = getLocalStore('Prompt')
+  const setPrompts = useSetAtom(promptsAtom)
+  const selectedPromptStorge = getLocalStore('selectedPrompt')
+  const [selected, setSelected] = useState(
+    selectedPromptStorge.basePrompt || promptStorge[0]?.basePrompt || ''
+  )
+  const [{ basePrompt }, setBasePrompt] = useAtom(settingsAtom)
+
+  const loadPrompt = () => {
+    const i = promptStorge.findIndex((p) => p.basePrompt === selected)
+
+    if (i === -1) {
+      return
+    }
+    setPrompts(promptStorge[i].promptsArray)
+    setSelected(promptStorge[i].basePrompt)
+    setBasePrompt({ basePrompt: promptStorge[i].basePrompt })
+    setLocalStore('selectedPrompt', promptStorge[i])
+  }
+
+  const delitePrompt = () => {
+    if (promptStorge.length === 0) {
+      return
+    }
+    console.log({ selected })
+    const prompts = promptStorge.filter(
+      (propt) => propt.basePrompt !== selected
+    )
+    console.log({ prompts })
+
+    setPrompts([])
+    setLocalStore('Prompt', prompts)
+    setLocalStore('selectedPrompt', { basePrompt: '', promptsArray: [] })
+  }
+
+  return (
+    <Modal opened={opened} onClose={close} title="Promps" centered>
+      <Tabs defaultValue="Load">
+        <Tabs.List>
+          <Tabs.Tab value="Load">Load</Tabs.Tab>
+          <Tabs.Tab value="Save prompt">Save prompt</Tabs.Tab>
+        </Tabs.List>
+
+        <Tabs.Panel value="Load">
+          <Flex direction="column" gap="lg">
+            <Text size="xl">Save prompt</Text>
+
+            <NativeSelect
+              disabled={promptStorge.length === 0}
+              value={selected}
+              onChange={(e) => setSelected(e.target.value)}
+              data={promptStorge.map((p) => p.basePrompt)}
+            />
+            <Flex gap="xl">
+              <Button variant="filled" onClick={loadPrompt}>
+                Load
+              </Button>
+              <Button variant="filled" color="red" onClick={delitePrompt}>
+                Delete
+              </Button>
+            </Flex>
+          </Flex>
+        </Tabs.Panel>
+
+        <Tabs.Panel value="Save prompt">Save prompt tab content</Tabs.Panel>
+      </Tabs>
+    </Modal>
+  )
+}
+
 const Main = () => {
   const img = useAtomValue(imgAtom)
   const [opened, { open, close }] = useDisclosure(false)
